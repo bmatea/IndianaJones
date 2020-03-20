67da65202201e4726b40b23e91584b0b2492c1cd
bmatea <mateaboban05@gmail.com>
2020-03-20 23:13:16 +0100

    NoviZahtjev editing

--- src/app/Aplikacije/TkSuglasnost/novi-zahtjev/novi-zahtjev.component.ts
+++ src/app/Aplikacije/TkSuglasnost/novi-zahtjev/novi-zahtjev.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-novi-zahtjev',
@@ -19,9 +20,53 @@ export class NoviZahtjevComponent implements OnInit {
   public trasa_opcine$;
   public trasa_poste$;
 
-  constructor() { }
+  form: FormGroup;
+  public korisnikType = true;
+
+  constructor(private builder: FormBuilder) { }
 
   ngOnInit() {
+    this.form = this.builder.group({
+      osobniPodaci: this.builder.group({
+        privatniKorisnik: ["privatni", []],
+        ime: ['', [Validators.required]],
+        prezime: ['', [Validators.required]],
+        naziv: [{value: '', disabled: true}, [Validators.required]],
+        dodatak: [{value: '', disabled: true}, [Validators.required]],
+        tel: ['', [Validators.required]],
+        email: ['', [Validators.required]],
+      }),
+      adresaZaDostavu: this.builder.group({
+        ime: ['', [Validators.required]],
+        prezime: ['', [Validators.required]],
+        naziv: [{value: '', disabled: true}, [Validators.required]],
+        dodatak: ['', [Validators.required]],
+        mjesto: ['', [Validators.required]],
+        ulica: ['', [Validators.required]],
+        kucniBr: ['', [Validators.required]],
+        dodatak2: ['', [Validators.required]],
+        napomena: ['', [Validators.required]],
+        opcina: ['', [Validators.required]],
+        posta: ['', [Validators.required]],
+      }),
+      podaciOLokaciji: this.builder.group({
+        tipSuglasnosti: ['', [Validators.required]],
+        kokc: ['', [Validators.required]],
+        svrha: ['', [Validators.required]],
+        mjesto: ['', [Validators.required]],
+        ulica: ['', [Validators.required]],
+        kucniBr: ['', [Validators.required]],
+        dodatak: ['', [Validators.required]],
+        napomena: ['', [Validators.required]],
+        opcina: ['', [Validators.required]],
+        posta: ['', [Validators.required]],
+      })
+
+    });
+  }
+
+  radioChange() {
+    console.log(this.form.get('osobniPodaci.privatniKorisnik'));
   }
 
 }
